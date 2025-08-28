6dc28a9cff812443ee1daa177c2665b7290c9636
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 14:49:08 -0400

    Se agrego la primera version de la seccion

--- components/Components/Section_Contactanos.tsx
+++ components/Components/Section_Contactanos.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+
+import SubSection_Formulario from "../Subcomponents/SubSection_Formulario";
+import SubSection_PrimerPaso from "../Subcomponents/SubSection_PrimerPaso";
 
 export default function Section_Contactanos() {
   return (
-    <div>Section_Contactanos</div>
-  )
+    <section className="flex flex-col bg-blue-600 py-10 px-30">
+      <h2 className="text-5xl font-semibold text-center text-white">Contactanos</h2>
+      <div className="flex w-full justify-around mt-10 items-center">
+        <SubSection_PrimerPaso />
+        <SubSection_Formulario />
+      </div>
+    </section>
+  );
 }
