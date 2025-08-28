30837730f79295d0881632433e2caa141c78a977
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 14:49:38 -0400

    Se agrego la primera version de la seccion Y la card se cmabiaran

--- components/Components/Section_NuestrosClientes.tsx
+++ components/Components/Section_NuestrosClientes.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Card_NuestrosClientes from "../Subcomponents/Card_NuestrosClientes";
 
 export default function Section_NuestrosClientes() {
   return (
-    <div>Section_NuestrosClientes</div>
-  )
+    <section className="flex flex-col py-10 px-30 gap-10">
+      <h2 className="text-5xl text-center font-semibold">Nuestros clientes lo confirman</h2>
+      <div className="flex justify-between">
+        <Card_NuestrosClientes />
+        <Card_NuestrosClientes />
+        <Card_NuestrosClientes />
+        <Card_NuestrosClientes />
+      </div>
+    </section>
+  );
 }
