7d9dc8e5b662b3226490dad29e54705e8081df09
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 15:20:36 -0400

    Se hicieron mejoras en el layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import React from "react";
 import Section_Header from "../../components/Components/Section_Header";
 import Section_Footer from "../../components/Components/Section_Footer";
+import "@/../style/global.css";
+import {  Poppins } from "next/font/google";
+
+// const albert = Albert_Sans({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ["300","400", "700", "800", "900"] });
 
 export default function layout({ children }: React.PropsWithChildren) {
   return (
-    <>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
-      <body>
-        <Section_Header />
-        {children}
-        <Section_Footer />
-      </body>
-    </>
+     <html lang="en">
+     <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+     </head>
+     <body className={poppins.className}>
+      <Section_Header />
+      {children}
+      <Section_Footer />
+     </body>
+     </html>
   );
 }
