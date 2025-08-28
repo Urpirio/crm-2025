37a314974e7b1c4925fef9626fdac4f55f567f10
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 14:48:55 -0400

    Se eliminaron recursos no necesarios

--- components/Components/Section_CentralizarClientes.tsx
+++ components/Components/Section_CentralizarClientes.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import Card_CentralizaClientes from "../Subcomponents/Card_CentralizaClientes";
 
 export default function Section_CentralizarClientes() {
   return (
-    <div>Section_CentralizarClientes</div>
-  )
+    <section className="px-30 py-10 flex flex-col gap-10">
+      <h2 className="text-5xl font-semibold mb-4 w-[60%]">
+        Centraliza tus clientes, potencia a tu equipo, impulsa tus ventas
+      </h2>
+      <div className="flex   gap-5 justify-between ">
+        <Card_CentralizaClientes />
+      </div>
+    </section>
+  );
 }
