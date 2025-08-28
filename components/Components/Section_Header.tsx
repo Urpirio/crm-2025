6b6fb01886a74bb1aa543d0e80263622ec3cdc4e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 16:16:13 -0400

    Se agregaron nuevas animaciones

--- components/Components/Section_Header.tsx
+++ components/Components/Section_Header.tsx
@@ -1,24 +1,35 @@
 import Image from "next/image";
 import Logo_Crm from "@/../public/Logo_Crm.svg";
 import Link from "next/link";
+import * as motion from "motion/react-client";
 
 export default function Section_Header() {
   return (
-    <section  className="w-full py-5 px-30 fixed z-50">
-      <header className="border flex justify-between items-center p-2  rounded-full bg-white/95 border-gray-200 shadow-md">
+    <section className="w-full py-5 px-30 fixed z-50">
+      <motion.header
+        animate={{ translateY:0 }}
+        style={{ translateY:-100 }}
+        className="border flex justify-between items-center p-2  rounded-full bg-white/95 border-gray-200 shadow-md"
+      >
         <div className="flex w-1/3">
-          <Image src={Logo_Crm} alt="Logo" className="h-10 w-10 object-contain" />
+          <Image
+            src={Logo_Crm}
+            alt="Logo"
+            className="h-10 w-10 object-contain"
+          />
         </div>
-        <nav className="flex w-1/3 justify-between" >
+        <nav className="flex w-1/3 justify-between">
           <Link href="">Inicio</Link>
           <Link href="">Sobre Nosotros</Link>
           <Link href="">Contactanos</Link>
           <Link href="">Reseñas</Link>
         </nav>
         <div className="flex  w-1/3 justify-end">
-          <button className="bg-primary text-white py-2 px-4 rounded-3xl">Empieza ahora</button>
+          <button className="bg-primary text-white py-2 px-4 rounded-3xl">
+            Empieza ahora
+          </button>
         </div>
-      </header>
+      </motion.header>
     </section>
   );
 }
