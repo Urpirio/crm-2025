cb33f580521bb4636ed495c6ab7aba27147b2756
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 16:16:22 -0400

    Se agregaron nuevas animaciones

--- components/Subcomponents/Card_CentralizaClientes.tsx
+++ components/Subcomponents/Card_CentralizaClientes.tsx
@@ -1,18 +1,21 @@
 import { Data_CardCentralizaClientes } from "../Data/Data_CardCentralizaClientes";
+import * as motion from "motion/react-client";
 
 export default function Card_CentralizaClientes() {
   const Cards = Data_CardCentralizaClientes.map(
     ({ id, title, description, icon }) => (
-      <article
+      <motion.article
+        whileInView={{ opacity: 1, translateY: 0 }}
+        initial={{ opacity: 0 , translateY: 100 }}
         key={id}
         className="flex flex-col justify-between items-start gap-2 w-[30%] bg-[#F0F2F5] p-5 rounded-xl shadow-2xl shadow-gray-100/50 border-gray-200"
       >
-          <div className="border border-primary rounded-2xl p-1 bg-primary">
-            {icon}
-          </div>
-          <h2 className="text-xl font-bold mt-2">{title}</h2>
-          <p className="  text-sm font-light">{description}</p>
-      </article>
+        <div className="border border-primary rounded-2xl p-1 bg-primary">
+          {icon}
+        </div>
+        <h2 className="text-xl font-bold mt-2">{title}</h2>
+        <p className="  text-sm font-light">{description}</p>
+      </motion.article>
     )
   );
   return Cards;
