5b947485bd09c00403bc09fc838e8e01c529d811
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 16:16:10 -0400

    Se agregaron nuevas animaciones

--- components/Components/Section_CentralizarClientes.tsx
+++ components/Components/Section_CentralizarClientes.tsx
@@ -1,11 +1,16 @@
 import Card_CentralizaClientes from "../Subcomponents/Card_CentralizaClientes";
+import * as motion from "motion/react-client";
 
 export default function Section_CentralizarClientes() {
   return (
-    <section className="px-30 py-10 flex flex-col gap-10">
-      <h2 className="text-5xl font-semibold mb-4 w-[60%]">
+    <section className="px-30 py-10 flex flex-col gap-10 ">
+      <motion.h2
+        whileInView={{ opacity: 1, translateY: 0 }}
+        initial={{ opacity: 0 }}
+        className="text-5xl font-semibold mb-4 w-[60%]"
+      >
         Centraliza tus clientes, potencia a tu equipo, impulsa tus ventas
-      </h2>
+      </motion.h2>
       <div className="flex   gap-5 justify-between ">
         <Card_CentralizaClientes />
       </div>
