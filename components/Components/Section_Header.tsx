0fd12778cef6b6d3e127439af4753a4a73802f8e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-28 14:49:15 -0400

    Se agrego la primera version de la seccion

--- components/Components/Section_Header.tsx
+++ components/Components/Section_Header.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import Logo_Crm from "@/../public/Logo_Crm.svg";
+import Link from "next/link";
 
 export default function Section_Header() {
   return (
-    <div>Section_Header</div>
-  )
+    <section  className="w-full py-5 px-30 fixed z-50">
+      <header className="border flex justify-between items-center p-2  rounded-full bg-white/95 border-gray-200 shadow-md">
+        <div className="flex w-1/3">
+          <Image src={Logo_Crm} alt="Logo" className="h-10 w-10 object-contain" />
+        </div>
+        <nav className="flex w-1/3 justify-between" >
+          <Link href="">Inicio</Link>
+          <Link href="">Sobre Nosotros</Link>
+          <Link href="">Contactanos</Link>
+          <Link href="">Reseñas</Link>
+        </nav>
+        <div className="flex  w-1/3 justify-end">
+          <button className="bg-primary text-white py-2 px-4 rounded-3xl">Empieza ahora</button>
+        </div>
+      </header>
+    </section>
+  );
 }
