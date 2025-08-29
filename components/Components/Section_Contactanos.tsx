406de521f2e7fafa4b74188d3ae48c347c3f32e4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-29 09:08:55 -0400

    Se hicieron mejoras en el estilo

--- components/Components/Section_Contactanos.tsx
+++ components/Components/Section_Contactanos.tsx
@@ -6,7 +6,7 @@ export default function Section_Contactanos() {
   return (
     <section className="flex flex-col bg-blue-600 py-10 lg:px-30">
       <h2 className="lg:text-5xl text-3xl font-semibold text-center text-white">Contactanos</h2>
-      <div className="flex flex-wrap w-full justify-around mt-10 items-center">
+      <div className="flex flex-wrap w-full justify-around mt-10 items-center gap-5">
         <SubSection_PrimerPaso />
         <SubSection_Formulario />
       </div>
