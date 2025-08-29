9f89fe9c7b5275b12ca83486560e5eba78cb8c43
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-29 14:36:27 -0400

    Se mejoraron los estilos

--- components/Components/Section_Contactanos.tsx
+++ components/Components/Section_Contactanos.tsx
@@ -4,7 +4,7 @@ import SubSection_PrimerPaso from "../Subcomponents/SubSection_PrimerPaso";
 
 export default function Section_Contactanos() {
   return (
-    <section className="flex flex-col bg-blue-600 py-10 lg:px-30">
+    <section className="flex flex-col bg-blue-600 py-10 lg:px-30" id="contactanos">
       <h2 className="lg:text-5xl text-3xl font-semibold text-center text-white">Contactanos</h2>
       <div className="flex flex-wrap w-full justify-around mt-10 items-center gap-5">
         <SubSection_PrimerPaso />
