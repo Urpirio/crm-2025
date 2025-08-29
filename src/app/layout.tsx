e8eda2a202e6f3505dae1559b3176a11dceaf54c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-29 09:09:24 -0400

    Se hicieron mejoras en el estilo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
+"use client";
 import Section_Header from "../../components/Components/Section_Header";
 import Section_Footer from "../../components/Components/Section_Footer";
 import "@/../style/global.css";
-import {  Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { useState } from "react";
 
 // const albert = Albert_Sans({ subsets: ["latin"] });
-const poppins = Poppins({ subsets: ["latin"], weight: ["300","400", "700", "800", "900"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "800", "900"],
+});
 
-export default function layout({ children }: React.PropsWithChildren) {
+export default function Layout({ children }: React.PropsWithChildren) {
+  const [MenuOpen, setMenuOpen] = useState<boolean>(false);
+
   return (
-     <html lang="en">
-     <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Document</title>
-     </head>
-     <body className={poppins.className}>
-      <Section_Header />
-      {children}
-      <Section_Footer />
-     </body>
-     </html>
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body className={poppins.className} onTouchMove={()=>setMenuOpen(false)}>
+        <Section_Header setMenuOpen={setMenuOpen} MenuOpen={MenuOpen} />
+        {children}
+        <Section_Footer />
+      </body>
+    </html>
   );
 }
